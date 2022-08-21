98e92842e557710f0f94c56afb1c1b41193d2cc6
Fagner Nunes <fagnernunes11@gmail.com>
2022-08-21 22:24:57 +0100

    first commit

---
+++ src/Reference.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author fagnernunes
+ */
+class Reference {
+    int id;
+    String year;
+    String place;
+    String title;
+    String edition;
+    String jornalTitle;
+    String Volume;
+    String autor;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setEdition(String edition) {
+        this.edition = edition;
+    }
+
+    public void setJornalTitle(String jornalTitle) {
+        this.jornalTitle = jornalTitle;
+    }
+
+    public void setVolume(String Volume) {
+        this.Volume = Volume;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+   
+
+    public int getId() {
+        return id;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getEdition() {
+        return edition;
+    }
+
+    public String getJornalTitle() {
+        return jornalTitle;
+    }
+
+    public String getVolume() {
+        return Volume;
+    }
+    
+    
+}
